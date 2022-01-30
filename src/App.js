f5f64da631c976a0890e960e1e712889506bcb6d
Pradyumna Karpuram <pkarpuram@twilio.com>
2022-01-30 14:33:13 -0800

    Move state management from react to Redux for Simple Hello world App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import {createStore} from "redux";
+import reducer from "./reducers"
 import HelloWorld from "./HelloWorld";
 
-const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+// const App = () => {
+//   const [tech] = useState("React");
+//   return <HelloWorld tech={tech} />;
+// };
+const initialState = {
+  tech: "React"
 };
+const store = createStore(reducer, initialState);
+class App extends Component {
+  render(){
+    return <HelloWorld tech={store.getState().tech}/>
+  }
+}
+
+
 
 export default App;
